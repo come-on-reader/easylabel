f4452082f42440be7907d8b4d9a613d487bda82b
ubuntu <zhubeihfut@163.com>
2023-12-30 19:42:46 +0800

    添加ShiroConfig

--- easylabel-shiro/src/main/java/cn/ustc/easylabelshiro/config/ShiroConfig.java
+++ easylabel-shiro/src/main/java/cn/ustc/easylabelshiro/config/ShiroConfig.java
@@ -1,8 +1,53 @@
 package cn.ustc.easylabelshiro.config;
 
+import cn.ustc.easylabelshiro.shiro.JwtFilter;
+import cn.ustc.easylabelshiro.shiro.MyRealm;
+import jakarta.servlet.Filter;
+import org.apache.shiro.mgt.DefaultSessionStorageEvaluator;
+import org.apache.shiro.mgt.DefaultSubjectDAO;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class ShiroConfig {
+    @Bean
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(DefaultWebSecurityManager defaultWebSecurityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        // 给filter设置安全管理器
+        shiroFilterFactoryBean.setSecurityManager(defaultWebSecurityManager);
+        // 默认认证界面-认证不通过时跳转
+        shiroFilterFactoryBean.setLoginUrl("/login");
 
+        // 添加自己的过滤器并取名为jwt
+        Map<String, Filter> filterMap = new HashMap<>();
+        filterMap.put("jwt", new JwtFilter());
+        shiroFilterFactoryBean.setFilters(filterMap);
+
+        // 配置系统受限资源
+        Map<String, String> urlMap = new HashMap<>();
+        urlMap.put("/**", "jwt"); // 所有的请求通过自己的过滤器
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(urlMap);
+
+        return shiroFilterFactoryBean;
+    }
+
+    // 创建安全管理器
+    @Bean
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(MyRealm realm) {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        // 给安全管理器设置Realm
+        securityManager.setRealm(realm);
+        // 关闭shiro的session，无状态方式使用session
+        DefaultSubjectDAO subjectDAO = new DefaultSubjectDAO();
+        DefaultSessionStorageEvaluator defaultSessionStorageEvaluator = new DefaultSessionStorageEvaluator();
+        defaultSessionStorageEvaluator.setSessionStorageEnabled(false);
+        subjectDAO.setSessionStorageEvaluator(defaultSessionStorageEvaluator);
+        securityManager.setSubjectDAO(subjectDAO);
+        return securityManager;
+    }
 }
